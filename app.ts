f00133cf5285c23da246f0ce415a827703292967
Irina Tkachenko Sergeevna <ira_t_s@mail.ru>
2018-10-02 01:21:10 +0300

    server update

--- app.ts
+++ app.ts
@@ -1,10 +1,9 @@
 import * as express from 'express'
+import { Request, Response, NextFunction } from 'express'
+import * as bodyParser from "body-parser"
 
 const app = express();
-
 const users: Array<any> = require('../users.json');
-const bodyParser: any = require("body-parser");
-const fs = require("fs");
 
 app.use(bodyParser());
 
@@ -13,72 +12,55 @@ app.get('/users', (req, res) => {
 });
 
 app.get('/users/:id', (req, res, next) => {
-	const userId = req.params["id"];
-	res.send(users.find(item => item.id === userId));
+  const userId = req.params["id"];
+  res.send(users.find(item => item.id == userId));
 });
 
 app.post('/users/add', (req, res, next) => {
   const user = {
-  	"id": Math.floor(Math.random() * 10000000000000001).toString(),
-	  "name": req.body.name,
-		"password": req.body.password,
-		"dateOfBirth": req.body.dateOfBirth||"1998-06-18T17:30:00.000Z",
-		"dateOfFirstLogin": (new Date).toISOString(),
-		"dateOfNextNotification": (new Date).toISOString(),
-		"information": req.body.information||"data"
+    id: Math.floor(Math.random() * 10000000000000001).toString(),
+    name: req.body.name,
+    password: req.body.password,
+    dateOfBirth: req.body.dateOfBirth,
+    dateOfFirstLogin: (new Date).toISOString(),
+    dateOfNextNotification: (new Date).toISOString(),
+    information: req.body.information
   }
   users.push(user);
-  fs.writeFile("../users.json", JSON.stringify(users), (err: any) => {
-  	if (err)
-  		next(err);
-  });
   res.send(user);
 });
 
 app.put('/users/:id', (req, res, next) => {
   const userId = req.params["id"];
-  let i = users.length;
-	while (i--) {
-  	if (users[i] && users[i].hasOwnProperty("id") &&
-  			(arguments.length > 2 && users[i]["id"] === userId)) { 
-   		users[i] = {
-   			"id": userId,
-			  "name": req.body.name||users[i].name,
-				"password": req.body.password||users[i].password,
-				"dateOfBirth": req.body.dateOfBirth||users[i].dateOfBirth,
-				"dateOfFirstLogin": req.body.dateOfFirstLogin||users[i].dateOfFirstLogin,
-				"dateOfNextNotification": req.body.dateOfNextNotification||users[i].dateOfNextNotification,
-				"information": req.body.information||users[i].information
-   		}
-  	}
-	}	
-	fs.writeFile("../users.json", JSON.stringify(users), (err: any) => {
-  	if (err)
-  		next(err);
-  });
+  const index = users.findIndex(item => item.id == userId);
+  if (index == -1) {
+    next(res);
+  }
+  users[index] = {
+    id: userId,
+    name: req.body.name||users[index].name,
+    password: req.body.password||users[index].password,
+    dateOfBirth: req.body.dateOfBirth||users[index].dateOfBirth,
+    dateOfFirstLogin: req.body.dateOfFirstLogin||users[index].dateOfFirstLogin,
+    dateOfNextNotification: req.body.dateOfNextNotification||users[index].dateOfNextNotification,
+    information: req.body.information||users[index].information
+  }
   res.send(users);
 });
 
 app.delete('/users/:id', (req, res, next) => {
   const userId = req.params["id"];
-  let i = users.length;
-	while (i--) {
-  	if (users[i] && users[i].hasOwnProperty("id") &&
-  			(arguments.length > 2 && users[i]["id"] === userId)) { 
-   		users.splice(i, 1);
-  	}
-	}
-	fs.writeFile("../users.json", JSON.stringify(users), (err: any) => {
-  	if (err)
-  		next(err);
-  });
+  const index = users.findIndex(item => item.id == userId);
+  if (index == -1) {
+    next(res);
+  }
+  users.splice(index, 1);
   res.send(users);
 });
 
-app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => { 
-	if (err.status !== 404) 
-		console.error(err); 
-	res.sendStatus(err.status || 500); 
+app.use((err: any, req: Request, res: Response, next: NextFunction) => { 
+  console.error(err.stack);
+  res.status(404).send('ID Not Found on Server');
 });
 
 app.listen(3030, () => {
